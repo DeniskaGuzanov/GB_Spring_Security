3050450329b8ae7bc18ef93cca3cc82fe37e3380
denis <xdenis@yandex.ru>
2024-02-06 19:03:45 +0300

    Initial commit

---
+++ src/test/java/com/example/GB_Spring_Security/GbSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GB_Spring_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
